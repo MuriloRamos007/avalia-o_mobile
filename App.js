baeb6273737c10735d264447b9fdcf11925baa14
Murilo Ramos <151067196+MuriloRamos007@users.noreply.github.com>
2025-04-11 19:25:34 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,37 +1,5 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text } from "react-native";
-import HomeScreen from "./scr/screens/HomeScreen";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <HomeScreen/>
-    );
-  }
+export default function App() {
+  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-    padding: 8,
-  },
-  paragraph: {
-    margin: 8,
-    fontSize: 16,
-    textAlign: "center",
-  },
-  h1: {
-    margin: 28,
-    fontSize: 36,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  h2: {
-    margin: 16,
-    fontSize: 28,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
