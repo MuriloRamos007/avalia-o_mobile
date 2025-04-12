89db71a129b88f4913eeeb35491ae333f79c6705
Murilo Ramos <151067196+MuriloRamos007@users.noreply.github.com>
2025-04-11 20:19:11 -0400

    Add files via upload

--- scr/screens/HomeScreen.js
+++ scr/screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function HomeScreen() {
+export default function HomeScreen({ route }) {
+  const { user } = route.params;
 
   return (
     <View style={styles.container}>
